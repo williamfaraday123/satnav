d3f10ef0cc09550e85793c35fac3856fbee4935d
williamfaraday123 <williamfaraday123@gmail.com>
2024-11-29 21:57:15 +0800

    update fetching without axios

--- client/src/services/TimeZoneService.jsx
+++ client/src/services/TimeZoneService.jsx
@@ -1,9 +1,8 @@
-import axios from "axios";
 
 const API_URL = `http://api.timezonedb.com/v2.1/get-time-zone`;
 const API_KEY = import.meta.env.VITE_TIMEZONEDB_API_KEY;
 
-export const getTimeZone = async (lat, lng) => {
+/* export const getTimeZone = async (lat, lng) => {
     try {
         const response = await axios.get(API_URL, {
             params: {
@@ -18,5 +17,32 @@ export const getTimeZone = async (lat, lng) => {
         return data;
     } catch (error) {
         console.error(error);
+        throw error;
+    }
+}; */
+
+export const getTimeZone = async (lat, lng) => {
+    const params = {
+        key: API_KEY,
+        format: 'json',
+        by: 'position',
+        lat: lat,
+        lng: lng
+    };
+    const queryString = new URLSearchParams(params).toString();
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+    try {
+        const response = await fetch(`${API_URL}?${queryString}`, requestOptions);
+        if (!response.ok) {
+            throw new Error('Network is not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
 };
