5d1ee87f354abf8ee8897a7df448d7376057d443
williamfaraday123 <williamfaraday123@gmail.com>
2025-02-08 02:14:32 +0800

    added routes

---
+++ client/src/pages/PlannedRoutes/page.jsx
@@ -0,0 +1,9 @@
+const PlannedRoutes = () => {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/d/embed?mid=15UtIyLOtZ3u6QV-ALKWJpR4KxaG5dco&ehbc=2E312F" width="640" height="480"></iframe>
+        </div>
+    );
+};
+
+export default PlannedRoutes;
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main/Main';
+import PlannedRoutes from '../pages/PlannedRoutes/page';
 
 const AppRoutes = () => {
     return(
         <Routes>
             <Route path = "/" Component = {Main} />
+            <Route path = "/planned-routes" Component = {PlannedRoutes} />
         </Routes>
     );
 };
